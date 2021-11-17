eaa8e38ed88923c8e3b3dac999db7272c27a3566
giocapeli <capeli.andreas@gmail.com>
2021-11-17 08:44:47 +0100

    checking heroku

--- routers/test.js
+++ routers/test.js
@@ -1,20 +1,8 @@
 const { Router } = require("express");
-const cheerio = require("cheerio");
-const axios = require("axios");
 
 const Ingredient = require("../models/").ingredient;
 const router = new Router();
 
 const url = "https://www.bbc.co.uk/food/search?q=almond";
-
-router.get("/", async (req, res, next) => {
-  try {
-    const resourse = await axios.get(url);
-    console.log(resourse.data);
-    res.send({ response: resourse.data });
-  } catch {
-    next(e);
-  }
-});
 
 module.exports = router;
